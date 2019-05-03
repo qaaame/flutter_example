f8d2ae1ea4ad530f41de7336ac7d8b754f6fb665
kameyama <hkame@jp.ibm.com>
2019-05-03 13:13:05 +0900

    update:Detailページを追加

--- beautiful_list/lib/main.dart
+++ beautiful_list/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:beautiful_list/model/lesson.dart';
 import 'package:flutter/material.dart';
+import 'package:beautiful_list/detail_page.dart';
 
 void main() => runApp(new MyApp());
 
@@ -16,7 +18,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class ListPage extends StatefulWidget {
   ListPage({Key key, this.title}) : super(key: key);
 
@@ -27,49 +28,79 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  List lessons;
+
+  @override
+  void initState() {
+    lessons = getLessons();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    ListTile makeListTile(Lesson lesson) => ListTile(
+      contentPadding:
+      EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      leading: Container(
+        padding: EdgeInsets.only(right: 12.0),
+        decoration: new BoxDecoration(
+            border: new Border(
+                right: new BorderSide(width: 1.0, color: Colors.white24))),
+        child: Icon(Icons.autorenew, color: Colors.white),
+      ),
+      title: Text(
+        lesson.title,
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
 
-    final makeListTile = ListTile(
-        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        leading: Container(
-          padding: EdgeInsets.only(right: 12.0),
-          decoration: new BoxDecoration(
-              border: new Border(
-                  right: new BorderSide(width: 1.0, color: Colors.white24))),
-          child: Icon(Icons.autorenew, color: Colors.white),
-        ),
-        title: Text(
-          "Introduction to Driving",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+      subtitle: Row(
+        children: <Widget>[
+          Expanded(
+              flex: 1,
+              child: Container(
+                // tag: 'hero',
+                child: LinearProgressIndicator(
+                    backgroundColor: Color.fromRGBO(209, 224, 224, 0.2),
+                    value: lesson.indicatorValue,
+                    valueColor: AlwaysStoppedAnimation(Colors.green)),
+              )),
+          Expanded(
+            flex: 4,
+            child: Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Text(lesson.level,
+                    style: TextStyle(color: Colors.white))),
+          )
+        ],
+      ),
+      trailing:
+      Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => DetailPage(lesson: lesson)));
+      },
+    );
 
-        subtitle: Row(
-          children: <Widget>[
-            Icon(Icons.linear_scale, color: Colors.yellowAccent),
-            Text(" Intermediate", style: TextStyle(color: Colors.white))
-          ],
-        ),
-        trailing:
-        Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
-
-    final makeCard = Card(
+    Card makeCard(Lesson lesson) => Card(
       elevation: 8.0,
       margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
       child: Container(
         decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-        child: makeListTile,
+        child: makeListTile(lesson),
       ),
     );
 
     final makeBody = Container(
+      // decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, 1.0)),
       child: ListView.builder(
         scrollDirection: Axis.vertical,
         shrinkWrap: true,
-        itemCount: 10,
+        itemCount: lessons.length,
         itemBuilder: (BuildContext context, int index) {
-          return makeCard;
+          return makeCard(lessons[index]);
         },
       ),
     );
@@ -101,7 +132,6 @@ class _ListPageState extends State<ListPage> {
         ),
       ),
     );
-
     final topAppBar = AppBar(
       elevation: 0.1,
       backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
@@ -114,14 +144,65 @@ class _ListPageState extends State<ListPage> {
       ],
     );
 
-
     return Scaffold(
       backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
       appBar: topAppBar,
       body: makeBody,
       bottomNavigationBar: makeBottom,
-
     );
   }
 }
 
+List getLessons() {
+  return [
+    Lesson(
+        title: "Introduction to Driving",
+        level: "Beginner",
+        indicatorValue: 0.33,
+        price: 20,
+        content:
+        "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Observation at Junctions",
+        level: "Beginner",
+        indicatorValue: 0.33,
+        price: 50,
+        content:
+        "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Reverse parallel Parking",
+        level: "Intermidiate",
+        indicatorValue: 0.66,
+        price: 30,
+        content:
+        "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Reversing around the corner",
+        level: "Intermidiate",
+        indicatorValue: 0.66,
+        price: 30,
+        content:
+        "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Incorrect Use of Signal",
+        level: "Advanced",
+        indicatorValue: 1.0,
+        price: 50,
+        content:
+        "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Engine Challenges",
+        level: "Advanced",
+        indicatorValue: 1.0,
+        price: 50,
+        content:
+        "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Self Driving Car",
+        level: "Advanced",
+        indicatorValue: 1.0,
+        price: 50,
+        content:
+        "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed.  ")
+  ];
+}
